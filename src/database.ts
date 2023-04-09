13ae4a8396c8015919f98e996a3c1299f68f328e
Roxy Pérez Polanco <88969908+roxy-perez@users.noreply.github.com>
2023-04-09 15:31:39 +0100

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -12,6 +12,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
+    dialectModule: mysql2,
     define: {
       underscored: true,
     },
